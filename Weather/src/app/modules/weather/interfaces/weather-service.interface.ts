b5cc60b4b7f708a88a3a9d3d0aacc27485cb2cee
German Lopez <gerleven@gmail.com>
2022-12-12 09:12:15 -0300

    add all response interfaces and test button on search page

--- Weather/src/app/modules/weather/interfaces/weather-service.interface.ts
+++ Weather/src/app/modules/weather/interfaces/weather-service.interface.ts
@@ -1,8 +1,68 @@
-export interface CoordinatesByNameInterface{
-    country: string
-    lat: string,
-    local_names: {},
-    lon: string,
-    name: string,
-    state: string
+export interface CoordinatesByNameInterface {
+  country: string;
+  lat: string;
+  local_names: {};
+  lon: string;
+  name: string;
+  state: string;
+}
+
+export interface CoordinatesByZipCodeInterface {
+  zip: string;
+  name: string;
+  lat: string;
+  lon: string;
+  country: string;
+}
+
+export interface CoordinatesRevertInterface {
+  name: string;
+  local_names: {};
+  lat: string;
+  lon: string;
+  country: string;
+  state: string;
+}
+
+export interface WeatherInterface {
+  coord: {
+    lon: string;
+    lat: string;
+  };
+  weather: {
+    id: string;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  base: string;
+  main: {
+    temp: string;
+    feels_like: string;
+    temp_min: string;
+    temp_max: string;
+    pressure: string;
+    humidity: string;
+    sea_level: string;
+    grnd_level: string;
+  };
+  visibility: string;
+  wind: {
+    speed: string;
+    deg: string;
+    gust: string;
+  };
+  clouds: {
+    all: string;
+  };
+  dt: string;
+  sys: {
+    country: string;
+    sunrise: string;
+    sunset: string;
+  };
+  timezone: string;
+  id: string;
+  name: string;
+  cod: string;
 }

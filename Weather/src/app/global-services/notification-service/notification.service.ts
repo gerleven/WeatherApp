0559d7c818618f978112fdda45ebf75307c0a0bc
German Lopez <gerleven@gmail.com>
2022-12-12 14:56:17 -0300

    test and and eventEmitter

--- Weather/src/app/global-services/notification-service/notification.service.ts
+++ Weather/src/app/global-services/notification-service/notification.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { NotificationSeverity } from 'src/app/global-enums/notification-sererity';
 import { ToastMessageInterface } from 'src/app/global-interfaces/toastMessage.interface';
+import { EventEmitter } from '@angular/core';
 
 
 @Injectable({
@@ -10,7 +11,10 @@ import { ToastMessageInterface } from 'src/app/global-interfaces/toastMessage.in
 export class NotificationService {
   
   //Since I'm using the MessageService (it need to be in the appComponent so all the children components can use it) in order to can wrapp it (for more decoupling), this emitter is for conect my service with MessageService.
-  // public toastEmitter = new EventEmitter<ToastMessageInterface>();
+  public toastEmitter = new EventEmitter<ToastMessageInterface>();
+  
+
+  
 
   public ShowNotification(toastMessage: ToastMessageInterface): void{
     // this.toastEmitter.emit(toastMessage);

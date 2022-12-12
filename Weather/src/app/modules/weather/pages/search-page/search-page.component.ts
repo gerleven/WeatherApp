dd0e8a0a73e9e74799757c7bdf6f97484ae2a864
German Lopez <gerleven@gmail.com>
2022-12-12 18:47:10 -0300

    services, methods and interfaces done

--- Weather/src/app/modules/weather/pages/search-page/search-page.component.ts
+++ Weather/src/app/modules/weather/pages/search-page/search-page.component.ts
@@ -1,52 +1,100 @@
 import { Component, OnInit } from '@angular/core';
 import { NotificationSeverity } from 'src/app/global-enums/notification-sererity';
 import { NotificationService } from 'src/app/global-services/notification-service/notification.service';
-import { CoordinatesByNameInterface, CoordinatesByNamePayload } from '../../interfaces/weather-service.interface';
+import {
+  CoordinatesByNameInterface,
+  CoordinatesByNamePayload,
+  CoordinatesByZipCodeInterface,
+  CoordinatesByZipCodePayload,
+  CoordinatesRevertInterface,
+  CoordinatesRevertPayload,
+  WeatherInterface,
+  WeatherPayload,
+} from '../../interfaces/weather-service.interface';
 import { WeatherService } from '../../services/weather.service';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
-  styleUrls: ['./search-page.component.css']
+  styleUrls: ['./search-page.component.css'],
 })
 export class SearchPageComponent implements OnInit {
+  constructor(
+    private weatherService: WeatherService,
+    private notificationService: NotificationService
+  ) {}
 
-  constructor( private weatherService: WeatherService, private notificationService: NotificationService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  private response:
+    | CoordinatesByNameInterface
+    | CoordinatesByZipCodeInterface
+    | CoordinatesRevertInterface
+    | WeatherInterface = {} as any;
+  private santafe= {
+    lat: '-31.6333',
+    lon: '-60.7',
+  };
 
-  private response: CoordinatesByNameInterface = {} as CoordinatesByNameInterface;
-
-  test(){
-    let payload: CoordinatesByNamePayload = {input: "Santa Fe,AR", limit: 1}
+  CallCoordinatesByName() {
+    let payload: CoordinatesByNamePayload = { input: 'Santa Fe,AR', limit: 1 };
     this.weatherService.GetCoordinatesByName(payload).subscribe(
-      (data)=>{this.response = data},
-      (error)=>{},
+      (data) => {
+        this.response = data;
+        console.log(this.response);
+      },
+      (error) => {}
+    );
+  }
+  CallCoordinatesByZipCode() {
+    let payload: CoordinatesByZipCodePayload = { zip_code: '3000,AR' };
+    this.weatherService.GetCoordinatesByZipCode(payload).subscribe(
+      (data) => {
+        this.response = data;
+        console.log(this.response);
+      },
+      (error) => {}
+    );
+  }
+  CallCoordinatesRevert() {
+    let payload: CoordinatesRevertPayload = { lat: this.santafe.lat, lon: this.santafe.lon };
+    this.weatherService.GetCoordinatesRevert(payload).subscribe(
+      (data) => {
+        this.response = data;
+        console.log(this.response);
+      },
+      (error) => {}
+    );
+  }
+  CallWeatherInterface() {
+    let payload: WeatherPayload = { lat: this.santafe.lat, lon: this.santafe.lon };
+    this.weatherService.GetWeather(payload).subscribe(
+      (data) => {
+        this.response = data;
+        console.log(this.response);
+      },
+      (error) => {}
     );
   }
 
-  test2(){
+  checkResponse() {
     this.response;
-    debugger
+    debugger;
   }
-  test3(){
+  showSeccessNotification() {
     this.notificationService.ShowNotification({
       severity: NotificationSeverity.success,
       message: 'Success test from search page',
-      details: "details..."
+      details: 'details...',
     });
   }
-  test4(){
+  showErrorNotification() {
     this.notificationService.ShowNotification({
       severity: NotificationSeverity.error,
       message: 'Error test from search page',
-      details: "details..."
+      details: 'details...',
     });
   }
 
-  showSuccess(){
-
-  }
-
+  showSuccess() {}
 }
--- Weather/src/app/modules/weather/services/weather.service.ts
+++ Weather/src/app/modules/weather/services/weather.service.ts
@@ -2,43 +2,67 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, map, Observable, throwError } from 'rxjs';
 import { tap } from 'rxjs/operators';
-import { CoordinatesByNameInterface, CoordinatesByNamePayload } from '../interfaces/weather-service.interface';
+import { NotificationSeverity } from 'src/app/global-enums/notification-sererity';
+import { NotificationService } from 'src/app/global-services/notification-service/notification.service';
+import { CoordinatesByNameInterface, CoordinatesByNamePayload, CoordinatesByZipCodeInterface, CoordinatesByZipCodePayload, CoordinatesRevertInterface, CoordinatesRevertPayload, WeatherInterface, WeatherPayload } from '../interfaces/weather-service.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WeatherService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient, private notificationService: NotificationService) {}
 
   private apiKey = 'b70c09c7012c40b88c45aa7daf0ee288';
 
   public GetCoordinatesByName(payload: CoordinatesByNamePayload): Observable<CoordinatesByNameInterface> {
-    let url: string = `http://api.openweathermap.org/geo/1.0/direct?q=${payload.input}&limit=${payload.limit}&appid=${this.apiKey}`;
+    let url: string = `http://api.openweathermap.org/geo/1.0/direct?q=${payload.input}&limit=${payload.limit?payload.limit:''}&appid=${this.apiKey}`;
     return this.httpClient.get<CoordinatesByNameInterface>(url).pipe(
       tap(
         (data) => {},
-        (error) => {}
+        (error) => {this.notificationService.ShowNotification({
+          severity: NotificationSeverity.error,
+          message: "Coordinates By Name couldn't be retrieved"
+        })}
       )
     );
   }
 
-  public GetCoordinatesByZipCOde(input: string,limit: number): Observable<CoordinatesByNameInterface> {
-    let url: string = `http://api.openweathermap.org/geo/1.0/zip?zip=3000,AR&appid=${this.apiKey}`;
-    return this.httpClient.get<CoordinatesByNameInterface>(url).pipe(
+  public GetCoordinatesByZipCode(payload: CoordinatesByZipCodePayload): Observable<CoordinatesByZipCodeInterface> {
+    let url: string = `http://api.openweathermap.org/geo/1.0/zip?zip=${payload.zip_code}&appid=${this.apiKey}`;
+    return this.httpClient.get<CoordinatesByZipCodeInterface>(url).pipe(
       tap(
         (data) => {},
-        (error) => {}
+        (error) => {this.notificationService.ShowNotification({
+          severity: NotificationSeverity.error,
+          message: "Coordinates By ZipCode couldn't be retrieved"
+        })}
       )
     );
   }
 
-  // GetCoordinatesByZipCOde
-  // http://api.openweathermap.org/geo/1.0/zip?zip=3000,AR&appid=${}
+  public GetCoordinatesRevert(payload: CoordinatesRevertPayload): Observable<CoordinatesRevertInterface> {
+    let url: string = `http://api.openweathermap.org/geo/1.0/reverse?lat=${payload.lat}&lon=${payload.lon}&limit${payload.limit?payload.limit:''}=&appid=${this.apiKey}`;
+    return this.httpClient.get<CoordinatesRevertInterface>(url).pipe(
+      tap(
+        (data) => {},
+        (error) => {this.notificationService.ShowNotification({
+          severity: NotificationSeverity.error,
+          message: "The City name for this Coordinates couldn't be retrieved"
+        })}
+      )
+    );
+  }
 
-  // GetCoordinatesRevert
-  // http://api.openweathermap.org/geo/1.0/reverse?lat=-31&lon=-60&limit=&appid={{API key}}
-
-
-  // GetWeather
-  // https://api.openweathermap.org/data/2.5/weather?lat=-31&lon=-60&appid={{API key}}&lang=es&mode=json&units=metric
+  public GetWeather(payload: WeatherPayload): Observable<WeatherInterface> {
+    let url: string = `https://api.openweathermap.org/data/2.5/weather?lat=${payload.lat}&lon=${payload.lon}&lang=${payload.lang?payload.lang:''}&mode=${payload.mode?payload.mode:''}&units=${payload.units?payload.units:''}&appid=${this.apiKey}`;
+    return this.httpClient.get<WeatherInterface>(url).pipe(
+      tap(
+        (data) => {},
+        (error) => {this.notificationService.ShowNotification({
+          severity: NotificationSeverity.error,
+          message: "Weather couldn't be retrieved"
+        })}
+      )
+    );
+  }
 }

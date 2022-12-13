354bce015d93bbb569b7ea805d09a0e9536c59b9
German Lopez <gerleven@gmail.com>
2022-12-13 12:54:23 -0300

    add test of call api in the start

--- Weather/src/app/modules/weather/pages/search-page/search-page.component.ts
+++ Weather/src/app/modules/weather/pages/search-page/search-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationSeverity } from 'src/app/global-enums/notification-sererity';
 import { NotificationService } from 'src/app/global-services/notification-service/notification.service';
-import { WeatherInterface } from '../../interfaces/weather-service.interface';
+import { WeatherInterface, WeatherPayload } from '../../interfaces/weather-service.interface';
 import { WeatherService } from '../../services/weather.service';
 
 @Component({
@@ -15,7 +16,37 @@ export class SearchPageComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.notificationService.ShowNotification({
+      severity: NotificationSeverity.info,
+      message: 'Testing Api...',
+      details: "It'll check for CORS and others problems"
+    });
+    setTimeout(this.testApi, 2000);
     
+  }
+
+  testApi(){
+    
+    let payload: WeatherPayload = {
+      lat: "-30",
+      lon: "-60"
+    } as WeatherPayload;
+    this.weatherService.GetWeather(payload).subscribe(
+      ()=>{
+        this.notificationService.ShowNotification({
+          severity: NotificationSeverity.success,
+          message: 'Api works...',
+        });
+      },
+      ()=>{
+        this.notificationService.ShowNotification({
+          severity: NotificationSeverity.success,
+          message: 'The call Api is not working...',
+          details: "Check network on devtools, if having CORS problems run in localy"
+        });
+      },
+      ()=>{},
+    );
   }
 
   favoriteList: WeatherInterface[] = [] as WeatherInterface[];
